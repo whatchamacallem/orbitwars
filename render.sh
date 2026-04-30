8e018d5ee5eec9d4be684480d13ecd6b9c2459db
A Johnston <ajohnston54637@gmail.com>
2026-04-30 12:10:07 -0700

    update render.sh

--- render.sh
+++ render.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-jupyter nbconvert --to html --execute orbitwars.ipynb --output /mnt/c/Users/ajohn/Downloads/orbitwars.html
+jupyter nbconvert --to html --execute orbitwars-test.ipynb --output /mnt/c/Users/ajohn/Downloads/orbitwars.html
